f144ae9be96e157cba9f639625b86be9cee464ae
Alexey Vasilyev <alexey@alexadotlife.com>
2019-08-18 15:31:07 +0200

    backward compatibility for export: check if db version <= 2.3.6, then disable export of write-hooks

--- rethinkdb/_export.py
+++ rethinkdb/_export.py
@@ -241,6 +241,8 @@ def export_table(db, table, directory, options, error_queue, progress_info, sind
     if options.outdated:
         run_options["read_mode"] = "outdated"
 
+    has_write_hooks = utils_common.check_minimum_version(options, '2.3.7', False)
+
     try:
         # -- get table info
 
@@ -257,13 +259,14 @@ def export_table(db, table, directory, options, error_queue, progress_info, sind
 
         sindex_counter.value += len(table_info["indexes"])
 
-        table_info['write_hook'] = options.retryQuery(
-            'table write hook data %s.%s' % (db, table),
-            query.db(db).table(table).get_write_hook(),
-            run_options=run_options)
+        if has_write_hooks:
+            table_info['write_hook'] = options.retryQuery(
+                'table write hook data %s.%s' % (db, table),
+                query.db(db).table(table).get_write_hook(),
+                run_options=run_options)
 
-        if table_info['write_hook'] is not None:
-            hook_counter.value += 1
+            if table_info['write_hook'] is not None:
+                hook_counter.value += 1
 
         with open(os.path.join(directory, db, table + '.info'), 'w') as info_file:
             info_file.write(json.dumps(table_info) + "\n")
